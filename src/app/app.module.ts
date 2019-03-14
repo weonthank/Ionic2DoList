2fa44c87db13345ca558822159e2dffbd03c2bb0
Will <willieonthank@gmail.com>
2019-03-14 10:31:16 -0700

    Commit 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,24 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule, AngularFireDatabase } from 'angularfire2/database';
+
+var config = {
+  apiKey: "AIzaSyA0MyE1yvGW3vYf8O-JaHOyhZYTgqC5YrY",
+  authDomain: "ionic2do-f7ddc.firebaseapp.com",
+  databaseURL: "https://ionic2do-f7ddc.firebaseio.com",
+  projectId: "ionic2do-f7ddc",
+  storageBucket: "ionic2do-f7ddc.appspot.com",
+  messagingSenderId: "955653044272"
+};
+//firebase.initializeApp(config);
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,
+    AngularFireModule.initializeApp(firebaseConfig), AngularFireDatabaseModule],
   providers: [
     StatusBar,
     SplashScreen,
--- src/app/tasklist/tasklist.page.ts
+++ src/app/tasklist/tasklist.page.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { NavController, AlertController, IonItemSliding } from '@ionic/angular';
+import { Task } from './task';
+import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
+import { Observable } from 'rxjs';
+
 
 @Component({
   selector: 'app-tasklist',
@@ -7,7 +11,7 @@ import { NavController, AlertController, IonItemSliding } from '@ionic/angular';
   styleUrls: ['./tasklist.page.scss'],
 })
 export class TasklistPage implements OnInit {
-  tasks: Array<any> = [];
+  tasks: Array<Task> = [];
   itemName: string;
 
   constructor(public navCtrl: NavController,public alertCtrl: AlertController) {
@@ -45,16 +49,18 @@ export class TasklistPage implements OnInit {
     prompt.present();
   }
 
-  markAsDone(slidingItem: ItemSliding, task: any) {
+  markAsDone(slidingItem: IonItemSliding, task: Task) {
     task.status = "done";
+    slidingItem.close();
   }
 
-  removeTask(slidingItem: ItemSliding,task: any) {
+  removeTask(slidingItem: IonItemSliding,task: Task) {
     task.status = "removed";
     let index = this.tasks.indexOf(task);
     if (index > -1) {
       this.tasks.splice(index, 1);
     }
+    slidingItem.close();
   }
   
   ngOnInit() {
